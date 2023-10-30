846aa32f0d12689f4380dc1aa5214abac7694e91
donwen <1359822154@qq.com>
2023-10-30 16:04:06 +0800

    init

---
+++ src/store/modules/user.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+export const useUserStore = defineStore('app-user', {
+  state: () => ({
+    count: 0,
+  }),
+  getters: {
+    double: (state) => state.count + 2,
+  },
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
+})
